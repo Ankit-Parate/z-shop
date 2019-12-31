10bd7771407942ce3d025d4c72c2f4309cd218ae
Ankit Parate <ankitparate279@gmail.com>
2019-12-31 09:24:23 +0530

    COMMIT 1: User: Ankit Parate

---
+++ DevOpsPrac/src/main/java/com/Ankit/AnkitJavaClass.java
@@ -0,0 +1,10 @@
+package com.Ankit;
+
+public class AnkitJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO ANKIT PARATE");
+	}
+
+}
